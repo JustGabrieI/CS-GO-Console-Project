28c16321e5b7d20a8e158cd1040abfdde90d659e
brayansekazvam <66061356+brayansekazvam@users.noreply.github.com>
2021-01-08 12:13:55 +0200

    Update MouseSettings.cs

--- CSGO Console/MouseSettings.cs
+++ CSGO Console/MouseSettings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -20,6 +20,50 @@ namespace CS_GO_Console
             else if (cmd == "m_customaccel 1")
             {
                 Console.WriteLine("activates the mouse boost.");
+            }
+            else if (cmd == "m_customaccel_scale 0.04")
+            {
+                Console.WriteLine("this command sets the mouse boost");
+            }
+            else if (cmd == "m_customaccel_exponent [value]")
+            {
+                Console.WriteLine("sets the index of the mouse boost");
+            }
+            else if (cmd == "m_customaccel_max [value]")
+            {
+                Console.WriteLine("the max possible index of the mouse boost");
+            }
+            else if (cmd == "m_mouseaccel1 1")
+            {
+                Console.WriteLine("activates the 2x mouse boost");
+            }
+            else if (cmd == "m_mouseaccel2 1")
+            {
+                Console.WriteLine("activates the 4x mouse boost");
+            }
+            else if (cmd == "m_mousespeed 1")
+            {
+                Console.WriteLine("the boost of the mouse will be managed by the Windows tools");
+            }
+            else if (cmd == "m_forward 1")
+            {
+                Console.WriteLine("the mouse sensitivity in moving forward");
+            }
+            else if (cmd == "m_side 0.8")
+            {
+                Console.WriteLine("the mouse sensitivity in moving sideways");
+            }
+            else if (cmd == "m_yaw 0.022")
+            {
+                Console.WriteLine("the mouse sensitivity in moving horizontally");
+            }
+            else if (cmd == "m_pich 0.022")
+            {
+                Console.WriteLine("the mouse sensitivity in moving vertically");
+            }
+            else if (cmd == "m_pitch 0.022")
+            {
+                Console.WriteLine("the command for inverting the mouse");
             }
         }
 
