e920445f98fc2582f272fb89970797280e09b402
Gabriel <65707182+JustGabrieI@users.noreply.github.com>
2021-01-13 01:05:13 +0200

    Update MouseSettings.cs

--- CSGO Console/MouseSettings.cs
+++ CSGO Console/MouseSettings.cs
@@ -11,59 +11,59 @@ namespace CS_GO_Console
         {
             if (cmd == "sensitivity 2.5")
             {
-                Console.WriteLine("sensitivity of the mouse; the value 2.5 is popular among pro-players.");
+                Console.WriteLine("Sensitivity of the mouse; the value 2.5 is popular among pro-players.");
             }
             else if (cmd == "m_rawinput 1")
             {
-                Console.WriteLine("with this command, the game will set the mouse directly, bypassing the operating system.");
+                Console.WriteLine("With this command, the game will set the mouse directly, bypassing the operating system.");
             }
             else if (cmd == "m_customaccel 1")
             {
-                Console.WriteLine("activates the mouse boost.");
+                Console.WriteLine("Activates the mouse boost.");
             }
             else if (cmd == "m_customaccel_scale 0.04")
             {
-                Console.WriteLine("this command sets the mouse boost");
+                Console.WriteLine("This command sets the mouse boost");
             }
             else if (cmd == "m_customaccel_exponent [value]")
             {
-                Console.WriteLine("sets the index of the mouse boost");
+                Console.WriteLine("Sets the index of the mouse boost");
             }
             else if (cmd == "m_customaccel_max [value]")
             {
-                Console.WriteLine("the max possible index of the mouse boost");
+                Console.WriteLine("The max possible index of the mouse boost");
             }
             else if (cmd == "m_mouseaccel1 1")
             {
-                Console.WriteLine("activates the 2x mouse boost");
+                Console.WriteLine("Activates the 2x mouse boost");
             }
             else if (cmd == "m_mouseaccel2 1")
             {
-                Console.WriteLine("activates the 4x mouse boost");
+                Console.WriteLine("Activates the 4x mouse boost");
             }
             else if (cmd == "m_mousespeed 1")
             {
-                Console.WriteLine("the boost of the mouse will be managed by the Windows tools");
+                Console.WriteLine("The boost of the mouse will be managed by the Windows tools");
             }
             else if (cmd == "m_forward 1")
             {
-                Console.WriteLine("the mouse sensitivity in moving forward");
+                Console.WriteLine("The mouse sensitivity in moving forward");
             }
             else if (cmd == "m_side 0.8")
             {
-                Console.WriteLine("the mouse sensitivity in moving sideways");
+                Console.WriteLine("The mouse sensitivity in moving sideways");
             }
             else if (cmd == "m_yaw 0.022")
             {
-                Console.WriteLine("the mouse sensitivity in moving horizontally");
+                Console.WriteLine("The mouse sensitivity in moving horizontally");
             }
             else if (cmd == "m_pich 0.022")
             {
-                Console.WriteLine("the mouse sensitivity in moving vertically");
+                Console.WriteLine("The mouse sensitivity in moving vertically");
             }
             else if (cmd == "m_pitch 0.022")
             {
-                Console.WriteLine("the command for inverting the mouse");
+                Console.WriteLine("The command for inverting the mouse");
             }
         }
 
